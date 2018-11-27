62f7067edccc67ff76ece4e9a1c20d05781bf218
Batman.qin <943104990@qq.com>
2018-11-27 11:50:22 +0800

    init

---
+++ open-api/src/test/java/com/demo/cloud/openapi/OpenApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demo.cloud.openapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OpenApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ service-hi/src/main/java/com/demo/cloud/servicehi/ServiceHiApplication.java
@@ -0,0 +1,26 @@
+package com.demo.cloud.servicehi;
+
+import com.demo.cloud.openapi.SchedualServiceHi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ServiceHiApplication implements SchedualServiceHi {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHiApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
+}
