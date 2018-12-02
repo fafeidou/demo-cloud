060fb77c7ebcc0db7ee049c21bb314d837401f48
author <943104990@qq.com>
2018-12-02 18:04:34 +0800

    gateway add oAuth2

--- service-zuul/src/main/java/com/demo/cloud/zuul/ServiceZuulApplication.java
+++ service-zuul/src/main/java/com/demo/cloud/zuul/ServiceZuulApplication.java
@@ -1,16 +1,29 @@
 package com.demo.cloud.zuul;
 
+import com.demo.cloud.zuul.config.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
 @EnableZuulProxy
+@EnableOAuth2Sso
 @SpringBootApplication
 public class ServiceZuulApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceZuulApplication.class, args);
+    }
+
+    /**
+     * 资源过滤器
+     * @return 资源过滤器
+     */
+    @Bean
+    public AccessFilter accessFilter(){
+        return new AccessFilter();
     }
 }
