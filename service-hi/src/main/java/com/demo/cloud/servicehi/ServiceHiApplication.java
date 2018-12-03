e088cf0c9f41d953cf29c4d29a4fb7f1ac2bd276
Batman.qin <943104990@qq.com>
2018-12-03 18:11:38 +0800

    exception 404

--- service-hi/src/main/java/com/demo/cloud/servicehi/ServiceHiApplication.java
+++ service-hi/src/main/java/com/demo/cloud/servicehi/ServiceHiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -20,6 +21,7 @@ public class ServiceHiApplication implements SchedualServiceHi {
     String port;
 
     @Override
+    @PreAuthorize("hasRole('ROLE_USER')")
     public String sayHiFromClientOne(String name) {
         return "hi " + name + " ,i am from port:" + port;
     }
